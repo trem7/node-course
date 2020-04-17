9afbe54165aafd3768f1267836677d495fb89c93
Adam <adam.tremblett7@gmail.com>
2020-04-17 11:34:53 -0400

    Registering routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,23 +8,20 @@ exports.getAddProduct = (req, res, next) => {
     productCSS: true, 
     activeAddProduct: true
   });
-}
+};
 
 exports.postAddProduct =  (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-}
+};
 
 exports.getProducts =  (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop/product-list', { 
-      prods: products, 
-      pageTitle: 'Shop', 
-      path: '/', 
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
     });
   });
 };
