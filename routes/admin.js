2ed9efac73fddf2e7ef69f6e3593abc036025674
Adam <adam.tremblett7@gmail.com>
2020-04-17 07:37:49 -0400

    Working on the layout with partials

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,13 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true})
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product', 
+    formsCSS: true, 
+    productCSS: true, 
+    activeAddProduct: true
+  })
 });
 
 router.post('/add-product', (req, res, next) => {
