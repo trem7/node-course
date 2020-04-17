d1deb022c158108cc7fa0d1a5309b9970cbbef94
Adam <adam.tremblett7@gmail.com>
2020-04-17 09:24:58 -0400

    Adding a product model

--- routes/admin.js
+++ routes/admin.js
@@ -4,24 +4,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
-const products = [];
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product', 
-    path: '/admin/add-product', 
-    formsCSS: true, 
-    productCSS: true, 
-    activeAddProduct: true
-  })
-});
+router.get('/add-product', productsController.getAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title});
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports= router;
