6553efebab3167fdc8d3661c4f7d2d3067d65fce
Adam <adam.tremblett7@gmail.com>
2020-04-22 14:21:32 -0400

    Updating products

--- routes/admin.js
+++ routes/admin.js
@@ -9,14 +9,14 @@ const adminController = require('../controllers/admin');
 // GET Routes
 router.get('/add-product', adminController.getAddProduct);
 
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 // POST Routes
 router.post('/add-product', adminController.postAddProduct);
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// router.post('/edit-product/', adminController.postEditProduct);
+router.post('/edit-product/', adminController.postEditProduct);
 
 // router.post('/delete-product/', adminController.postDeleteProduct);
 
