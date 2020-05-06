8dad3ead719cbe2c9e13e3a57c6580960398cf67
Adam <adam.tremblett7@gmail.com>
2020-05-06 09:27:12 -0400

    Handling file uploads with multer

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 require('dotenv').config();
 
 const errorController = require('./controllers/error');
@@ -29,6 +30,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({dest: 'images'}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
